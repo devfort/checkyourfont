7025319b01cdd647a7a6aa8a8743ba21eca9165e
James Aylett <james@tartarus.org>
2013-10-09 16:36:22 +0100

    ;text uses lorem ipsum (to check line metrics).

--- fontly/views.py
+++ fontly/views.py
@@ -62,6 +62,8 @@ class Font(TemplateView):
             elif name.startswith('font.'):
                 _, font = name.split('.', 2)
                 kwargs['webfonts'][font] = value
+            elif name == 'text':
+                kwargs['text'] = """Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."""
 
         if sizes is None:
             if min_size is None:
